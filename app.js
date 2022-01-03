5798c3678fd157d09487bc5abd2c4608e8ed074c
Andrew Ananenko <pocketbook.love24@gmail.com>
2022-01-03 19:30:41 +0300

    Data stuctures. Linked list, binary tree, hash table.

---
+++ app.js
@@ -0,0 +1,271 @@
+// Linked list
+
+/* function LinkedList() {
+  this.head = null
+  this.tail = null
+}
+
+function Node(value, next, prev) {
+  this.value = value
+  this.next = next
+  this.prev = prev
+}
+
+
+LinkedList.prototype.addToHeadList = function (value) {
+  let newNode = new Node(value, this.head, null)
+  if (this.head) this.head.prev = newNode
+  else this.tail = newNode
+
+  this.head = newNode
+}
+
+LinkedList.prototype.addToTailList = function (value) {
+  let newNode = new Node(value, null, this.tail)
+  if (this.head) this.tail.next = newNode
+  else this.tail = newNode
+
+  this.tail = newNode
+}
+
+LinkedList.prototype.removeLastNode = function () {
+  if (!this.tail) return null
+  let val = this.tail.value
+  if (this.head == this.tail) {
+    this.head = this.tail = null
+  } else {
+    let temp = this.tail.prev
+    temp.next = null
+    this.tail = null
+    this.tail = temp
+  }
+  return val
+}
+
+LinkedList.prototype.removeFirstNode = function () {
+  if (!this.head) return null
+  let val = this.head.value
+  this.head = this.head.next
+  if (this.head) this.head.prev = null
+  else this.tail = null
+  return val
+}
+
+LinkedList.prototype.removeLastNode = function () {
+  if (!this.tail) return null
+  let val = this.tail.value
+  this.tail = this.tail.prev
+  if (this.tail) this.tail.next = null
+  else this.head = null
+  return val
+}
+
+
+LinkedList.prototype.listHasValue = function (value) {
+  let currentNode = this.head
+  while (currentNode) {
+    if (currentNode.value == value) return true
+    currentNode = currentNode.next
+  }
+  return false
+}
+
+LinkedList.prototype.indexOf = function (value) {
+  let currentNode = this.head
+  let iter = 0
+  let indexes = []
+  while (currentNode) {
+    if (currentNode.value == value) indexes.push(iter)
+    currentNode = currentNode.next
+    iter++
+  }
+  return indexes
+}
+
+let ll = new LinkedList()
+ll.addToHeadList(300)
+ll.addToHeadList(200)
+ll.addToHeadList(100)
+ll.addToTailList(400)
+ll.addToTailList(100)
+ll.addToTailList(500)
+
+
+console.log(ll.listHasValue(200)) // true
+console.log(ll.listHasValue(600)) // false
+console.log(ll) // [100,200,300,400,100,500]
+console.log(ll.indexOf(100)) // [0,4] */
+
+// Binary search
+
+/* function BST(value) {
+  this.value = value
+  this.left = null
+  this.right = null
+}
+function log(value) {
+  console.log('val : ', value)
+}
+
+BST.prototype.contains = function (value) {
+  if (value == this.value) return true
+  let turn = value > this.value ? 'right' : 'left'
+
+  if (turn == 'right' && this.right) return this.right.contains(value)
+  else if (turn == 'left' && this.left) return this.left.contains(value)
+  else return false
+}
+
+BST.prototype.insert = function (value) {
+  if (value >= this.value) {
+    if (!this.right) this.right = new BST(value)
+    else this.right.insert(value)
+  }
+  if (value < this.value) {
+    if (!this.left) this.left = new BST(value)
+    else this.left.insert(value)
+  }
+}
+
+BST.prototype.depthTraversal = function (iterator, typeOrder) {
+
+  if (typeOrder == 'pre-order') {
+    // pre-order
+    iterator(this.value)
+    if (this.left) this.left.depthFirstTraversal(iterator, typeOrder)
+    if (this.right) this.right.depthFirstTraversal(iterator, typeOrder)
+  }
+  else if (typeOrder == 'post-order') {
+    // post-order
+    if (this.left) this.left.depthFirstTraversal(iterator, typeOrder)
+    if (this.right) this.right.depthFirstTraversal(iterator, typeOrder)
+    iterator(this.value)
+  } else {
+    // asc
+    if (this.left) this.left.depthFirstTraversal(iterator, typeOrder)
+    iterator(this.value)
+    if (this.right) this.right.depthFirstTraversal(iterator, typeOrder)
+  }
+}
+
+BST.prototype.breadthTraversal = function (iterator) {
+  const queue = [this]
+  function traversalFn(root) {
+    if (root.left) queue.push(root.left)
+    if (root.right) queue.push(root.right)
+
+    if (queue.length) {
+      let nextNode = queue.shift()
+      log(nextNode.value)
+      traversalFn(nextNode)
+    }
+  }
+  traversalFn(this)
+}
+
+BST.prototype.getMinVal = function () {
+  if (!this.left) return this.value
+  return this.left.getMinVal()
+}
+
+BST.prototype.getMaxVal = function () {
+  if (!this.right) return this.value
+  return this.right.getMaxVal()
+}
+
+let root = new BST(50)
+root.insert(30)
+root.insert(70)
+root.insert(45)
+root.insert(20)
+root.insert(10)
+root.insert(35)
+root.insert(60)
+root.insert(100)
+root.insert(85)
+root.insert(105)
+root.insert(59)
+
+root.depthFirstTraversal(log, 'post-order')
+root.breadthTraversal(log)
+console.log(root.contains(105))
+console.log(root.getMinVal())
+console.log(root.getMaxVal()) */
+
+// Hash table
+
+/* function HashTable(size) {
+  this.bucket = new Array(size)
+  this.bucketSize = size
+}
+
+function HashNode(key, value, next = null) {
+  this.key = key
+  this.value = value
+  this.next = next
+}
+HashTable.prototype.hash = function (key) {
+  let hash = 0
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i)
+  }
+  return hash % this.bucketSize
+}
+HashTable.prototype.insert = function (key, value) {
+  let node = new HashNode(key, value)
+  let hash = this.hash(key)
+
+  if (!this.bucket[hash]) this.bucket[hash] = node
+  else {
+    let currentNode = this.bucket[hash]
+    while (currentNode) {
+      if (currentNode.key == key) {
+        currentNode.value = value
+        return
+      }
+
+      if (!currentNode.next) currentNode.next = node
+      currentNode = currentNode.next
+    }
+  }
+}
+HashTable.prototype.get = function (key) {
+  let hash = this.hash(key)
+  if (!this.bucket[hash]) return null
+  else {
+    let currentNode = this.bucket[hash]
+    while (currentNode) {
+      if (currentNode.key == key) return currentNode.value
+      currentNode = currentNode.next
+    }
+    return 'none'
+  }
+}
+HashTable.prototype.retrieveAll = function () {
+  let pairs = []
+  function toPair(node) {
+    pairs.push(`${node.key} : ${node.value}`)
+  }
+  for (let node of this.bucket) {
+    while (node) {
+      toPair(node)
+      node = node.next
+    }
+  }
+  return pairs
+}
+
+let table = new HashTable(30)
+table.insert('Dane', 'dane@gmail.com')
+table.insert('Andrew', 'andrew@gmail.com')
+table.insert('Daen', 'daen@gmail.com')
+table.insert('Dnae', 'Dnae@gmail.com')
+
+table.insert('Andrew', 'future@gmail.com')
+table.insert('Daen', 'EDITED DAEN')
+// console.log(table)
+// console.log(table.retrieveAll())
+ */
+// Algorithmes
+
+// Fizz-Buzz
