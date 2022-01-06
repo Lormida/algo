2731e95c076d32b0ca2d2c0e7d36db8d66759c5d
Andrew Ananenko <pocketbook.love24@gmail.com>
2022-01-06 05:03:04 +0300

    dijkstra s algorithm

--- app.js
+++ app.js
@@ -609,4 +609,45 @@ console.log(breadthSearch(graph, 'a', 'e')) */
 
 console.log(quickSort([7, 3, -100, 10, 1, -14, 18, 1, 2, 0, 9, 11, -4])) */
 
+// Find the shortest way (Dijctra)
+
+/* const graph = {}
+graph.a = { b: 2, c: 1 }
+graph.b = { f: 7 }
+graph.c = { d: 5, e: 2 }
+graph.d = { f: 2 }
+graph.e = { f: 150 }
+graph.f = { g: 1 }
+graph.g = {}
+
+// graph.a = { b: 10, e: 30, d: 100 }
+// graph.b = { c: 50 }
+// graph.c = { d: 10 }
+// graph.e = { c: 20, d: 60 }
+// graph.d = {}
+
+function findShortestWay(graph, start, end, neighbors = {}, passed = []) {
+  // Write info about close neighbors
+  Object.keys(graph[start]).forEach(point => {
+    // If new way faster than old - replace it
+    if (((neighbors[start] || 0) + graph[start][point]) < (neighbors[point] || Number.POSITIVE_INFINITY)) {
+      neighbors[point] = (neighbors[start] || 0) + graph[start][point]
+    }
+  })
+  // Check current point as passed
+  passed.push(start)
+
+  // Find way to every neighbor
+  Object.keys(neighbors).forEach(point => {
+    // This point weren't passed yet
+    if (!passed.includes(point)) {
+      findShortestWay(graph, point, end, neighbors, passed)
+    }
+  })
+  return neighbors
+}
+
+
+console.log(findShortestWay(graph, 'a', 'd')) */
+
 //
